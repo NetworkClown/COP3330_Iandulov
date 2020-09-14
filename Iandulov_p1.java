a252dacd152fdf95ff96a52e1757fc9550d70aad
Kirill Iandulov <70071678+NetworkClown@users.noreply.github.com>
2020-09-14 17:09:05 -0400

    Update Iandulov_p1.java

--- Iandulov_p1.java
+++ Iandulov_p1.java
@@ -3,22 +3,22 @@ public class Encryption
 { 
 public static String encrypt(String number) 
 {
-int arr[] = new int[4];
+int arr[]=new int[4];
 for(int i=0; i<4; i++) 
 {
-char ch = number.charAt(i);
-arr[i] = Character.getNumericValue(ch);
+char ch=number.charAt(i);
+arr[i]=Character.getNumericValue(ch);
 }
 
 for(int i=0; i<4; i++) 
 {
-int temp =arr[i] ;
-temp += 7 ;
-temp = temp%10 ;
-arr[i] = temp ;
+int temp=arr[i];
+temp+=7;
+temp=temp%10;
+arr[i]=temp;
 }
-int temp = arr[0];
-arr[0] = arr[2];
+int temp=arr[0];
+arr[0]=arr[2];
 arr[2]=temp;
 temp =arr[1];
 arr[1]=arr[3];
